7f8b8305ee310b94c32e48336706a7c6d2f2ffdd
debojyoti <debojyoti.js@gmail.com>
2019-07-18 23:25:37 +0530

    Modified sample config

---
+++ src/app.config.sample.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  "apiKey": "xxxxxxxxxxxxxxxxxxxxxxxx",
+  "authDomain": "xxxxxxxxxxxxxxxxxxxxxxxx",
+  "databaseURL": "xxxxxxxxxxxxxxxxxxxxxxxx",
+  "projectId": "xxxxxxxxxxxxxxxxxxxxxxxx",
+  "storageBucket": "",
+  "messagingSenderId": "xxxxxxxxxxxxxxxxxxxxxxxx",
+  "appId": "xxxxxxxxxxxxxxxxxxxxxxxx"
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { firebaseConfig } from 'src/app.config';
 
 @NgModule({
   declarations: [
